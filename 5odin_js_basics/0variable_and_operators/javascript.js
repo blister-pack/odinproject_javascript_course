22fb9339931191cfcbc906d7208f01195ea2ceb7
blister-pack <andrecrujo@hotmail.com>
2024-03-24 21:43:39 +0100

    variables and operators lesson complete

--- 5odin_js_basics/0variable_and_operators/javascript.js
+++ 5odin_js_basics/0variable_and_operators/javascript.js
@@ -15,4 +15,18 @@ const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
 twoDecimalPlaces;
 
 let myNumber = "74";
-myNumber =  Number(myNumber) + 3;
+myNumber = Number(myNumber) + 3;
+
+console.log(23 + 97)
+console.log(8 + 7 + 2 + 5 + 1 + 8)
+console.log((4 + 6 + 9) / 77)
+
+let a = 10
+console.log(a)
+
+let b = 7 * a
+console.log(b)
+
+let MAX = 57
+let actual = MAX - 13
+let percentage = actual / MAX
