60a2f634203f0158917aba06fad1a07b7b027ede
blister-pack <andrecrujo@hotmail.com>
2024-03-24 21:22:25 +0100

    javascript tests

--- 5odin_js_basics/0variable_and_operators/javascript.js
+++ 5odin_js_basics/0variable_and_operators/javascript.js
@@ -3,3 +3,16 @@ console.log("ahahhah");
 let x = "lol";
 
 document.getElementById("demo").innerHTML = x;
+
+const myInt = 5;
+const myFloat = 6.667;
+myInt;
+myFloat;
+
+const lotsOfDecimal = 1.766584958675746364;
+lotsOfDecimal;
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+twoDecimalPlaces;
+
+let myNumber = "74";
+myNumber =  Number(myNumber) + 3;
