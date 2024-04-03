7f2cc9b707f8afdd573cb98aa4e4ee61b0db7657
blister-pack <andrecrujo@hotmail.com>
2024-04-03 12:05:29 +0200

    first func idea

--- 5odin_js_basics/4rock_paper_scissors_project/script.js
+++ 5odin_js_basics/4rock_paper_scissors_project/script.js
@@ -1,10 +1,19 @@
 // logic for a Rock, Paper, Scissors game
 
+moves_list = ["rock", "paper", "scissors"]
+
 function getComputerChoice() {
     // this function returns the choice of the computer (random)
-
+    let choice = Math.floor(Math.random() * 3);
+    // 0 is rock; 1 is paper; 2 is scissors
+    return choice;
 }
 
 function roundPlay(playerSelection, computerSelection) {
     // this function returns the winner of the round
+}
+
+function playGame(params) {
+    // this function plays a game of janken with 5 rounds
+    
 }
