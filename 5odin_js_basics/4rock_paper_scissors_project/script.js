86b237ab06cb7e24b380dab2352732e514768557
blister-pack <andrecrujo@hotmail.com>
2024-04-03 15:47:26 +0200

    completed function that writes the round result

--- 5odin_js_basics/4rock_paper_scissors_project/script.js
+++ 5odin_js_basics/4rock_paper_scissors_project/script.js
@@ -23,8 +23,32 @@ function roundPlay(playerSelection, computerSelection) {
     // this function returns the winner of the round
     if (playerSelection == computerSelection) {
         // draw
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        // win
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        // win
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        // win
+    } else {
+        // lose
+        return `You lose! ${computerSelection}`
     }
 
+}
+
+function roundResultMessage(win = true, playerSelection, computerSelection) {
+    // this is just meant to capitalize the moves, yes this could have probably been avoided
+    let formatted_playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+    let formatted_computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+
+
+    if (win === true) {
+        // victory message
+        return `You win! ${formatted_playerSelection} beats ${formatted_computerSelection}!`;
+    } else {
+        // losing message
+        return `You lose! ${formatted_computerSelection} beats ${formatted_playerSelection}!`;
+    }
 }
 
 function playGame(params) {
