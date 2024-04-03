33b994149fed92af5ef55fc846a0f8c7072f4dfb
blister-pack <andrecrujo@hotmail.com>
2024-04-03 13:55:51 +0200

    function that formats and validates a user's move

--- 5odin_js_basics/4rock_paper_scissors_project/script.js
+++ 5odin_js_basics/4rock_paper_scissors_project/script.js
@@ -1,6 +1,16 @@
 // logic for a Rock, Paper, Scissors game
 
-moves_list = ["rock", "paper", "scissors"]
+let moves_list = ["rock", "paper", "scissors"];
+
+function getUserMove() {
+    let player_move = prompt("Rock, paper or scissors? Choose your move!");
+    let formatted_player_move = player_move.toLowerCase();
+    if (moves_list.includes(formatted_player_move)) {
+        return formatted_player_move;
+    } else {
+        alert("Your move is not valid. Refresh to try again.")
+    }
+}
 
 function getComputerChoice() {
     // this function returns the choice of the computer (random)
@@ -17,3 +27,6 @@ function playGame(params) {
     // this function plays a game of janken with 5 rounds
     
 }
+
+
+getUserMove()
