f396c8b64d712337788091bc50b37fd6598204a9
blister-pack <andrecrujo@hotmail.com>
2024-04-04 15:08:42 +0200

    game works
    the game is working now, it plays 5 rounds and the game stops after those 5 rounds. at the end of each round you can see what the player chose, the pc chose and what the score is

--- 5odin_js_basics/4rock_paper_scissors_project/script.js
+++ 5odin_js_basics/4rock_paper_scissors_project/script.js
@@ -6,7 +6,7 @@ function getUserMove() {
     let player_move = prompt("Rock, paper or scissors? Choose your move!");
     let formatted_player_move = player_move.toLowerCase();
     if (moves_list.includes(formatted_player_move)) {
-        console.log("player:" + formatted_player_move);
+        console.log("player: " + formatted_player_move);
         return formatted_player_move;
     } else {
         alert("Your move is not valid. Refresh to try again.");
@@ -26,12 +26,15 @@ function roundPlay(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         // draw
         console.log(`That's a draw, the player and the computer chose the same move!`);
+        return `draw`;
     } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
         // win
         console.log(roundResultMessage(true, playerSelection, computerSelection));
+        return `win`;
     } else {
         // lose
         console.log(roundResultMessage(false, playerSelection, computerSelection));
+        return `lose`
     }
 
 }
@@ -55,13 +58,16 @@ function playGame() {
     // this function plays a game of janken with 5 rounds
     let player_score = 0;
     let pc_score = 0;
+    // let round_end_message = (roundPlay(getUserMove(), getComputerChoice()));
+    // console.log(round_end_message);
 
     for (let index = 0; index < 5; index++) {
         // each loop is a round
-        let round_end_message = roundPlay(getUserMove(), getComputerChoice());
+        let round_end_message = (roundPlay(getUserMove(), getComputerChoice()));
+        console.log(round_end_message);
 
         if (round_end_message.includes("draw") === true) {
-            break;
+            continue;
         } else if (round_end_message.includes("win") === true) {
             player_score++;
         } else if (round_end_message.includes("lose") === true) {
@@ -71,8 +77,8 @@ function playGame() {
         console.log(`PLAYER SCORE: ${player_score}`);
         console.log(`PC SCORE: ${pc_score}`);
     }
-    
-}
+}    
+
 
 
 // roundPlay(getUserMove(), getComputerChoice())
