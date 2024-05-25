fb333c06e13fe5cfb4ee7162435836d922bcde21
blister-pack <andrecrujo@hotmail.com>
2024-05-25 15:55:14 +0200

    js exercise now works EXACTLY as intended
    0 is a number lower than 100 so it's not supposed to break the loop; pressing cancel should break the loop because it returns null

--- 5odin_js_basics/5arrays_and_loops/repeat_correct_input/script.js
+++ 5odin_js_basics/5arrays_and_loops/repeat_correct_input/script.js
@@ -1,8 +1,8 @@
 let right_number = false;
 
 while (right_number == false) {
-    let user_number = prompt("Gib number :D")
-    if (parseInt(user_number) > 100 || user_number == "") {
+    let user_number = prompt("Gib number :D", "0");
+    if (user_number===null || parseInt(user_number) > 100 || user_number == "") {
         right_number = true;
     } else {
         continue
