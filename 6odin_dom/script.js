d4ad86c8cd2ea00a8e3e23502ec3bcc079974c9c
blister-pack <andrecrujo@hotmail.com>
2024-05-30 19:24:33 +0200

    basic dom manipulation exercise complete

--- 6odin_dom/script.js
+++ 6odin_dom/script.js
@@ -9,3 +9,17 @@ container.appendChild(redParagraph);
 
 const blueH3 = document.createElement("h3");
 blueH3.textContent = "I'm a blue h3!"
+blueH3.style.cssText = "color:blue;";
+container.appendChild(blueH3);
+
+const weirdDiv = document.createElement("div");
+weirdDiv.style.cssText = "background:pink; border-style:solid; border-color:black; border-width:3px;";
+container.appendChild(weirdDiv);
+
+const weirdH1 = document.createElement("h1");
+weirdH1.textContent = "I'm in a div";
+weirdDiv.appendChild(weirdH1);
+
+const weirdP = document.createElement("p");
+weirdP.textContent = "ME TOO!";
+weirdDiv.appendChild(weirdP);
