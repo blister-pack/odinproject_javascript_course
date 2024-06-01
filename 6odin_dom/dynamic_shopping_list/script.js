8c5fca547c511395b68a547b687bd06495bf89b6
blister-pack <andrecrujo@hotmail.com>
2024-06-01 12:54:30 +0200

    nothing works and idk how to make the delete work, debugging

--- 6odin_dom/dynamic_shopping_list/script.js
+++ 6odin_dom/dynamic_shopping_list/script.js
@@ -5,7 +5,19 @@ const itemList = document.querySelector("ul");
 const addItem = function() {
     let itemName = input.textContent;
     input.textContent = "";
-    
+
+    const itemLi = document.createElement("li");
+    const itemSpan = document.createElement("span");
+    const itemBtn = document.createElement("button");
+
+    itemLi.appendChild(itemSpan);
+    itemLi.appendChild(itemBtn);
+
+    itemSpan.textContent = itemName;
+    itemBtn.textContent = "Delete Item";
+
+    itemList.appendChild(itemLi);
+    focus(input);
 };
 
 btn.addEventListener("click", addItem);
