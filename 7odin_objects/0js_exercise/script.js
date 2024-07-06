825321d45781f0dd94f90a747bdeed99394b79c4
blister-pack <andrecrujo@hotmail.com>
2024-07-06 18:43:39 +0200

    exercise 3 complete

--- 7odin_objects/0js_exercise/script.js
+++ 7odin_objects/0js_exercise/script.js
@@ -22,3 +22,22 @@ let schedule = {};
 console.log((isEmpty(schedule))); // should return true
 schedule["wakeUp"] = "8:30";
 console.log((isEmpty(schedule))); // should return false
+
+// exercise 3
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+};
+
+function countSalary(allSalaries) {
+    // this function counts the salaries of the allSalaries
+    let salarySum = 0;
+    for (let salary in allSalaries) {
+        salarySum += allSalaries[salary];
+    }
+    return salarySum;
+}
+
+console.log(countSalary(salaries));
