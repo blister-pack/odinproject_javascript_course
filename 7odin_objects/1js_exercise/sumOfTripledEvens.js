6bacfda3d5f98955d8143220e5a2da1055d86e4c
blister-pack <andrecrujo@hotmail.com>
2024-07-07 16:59:38 +0200

    rewriting functions as a way to understand map, filter and reduce

---
+++ 7odin_objects/1js_exercise/sumOfTripledEvens.js
@@ -0,0 +1,14 @@
+// the objective of this exercise is to rewrite the function
+// using filter, map and reduce
+
+function mapSumOfTripledEvents(params) {
+    // func using map
+}
+
+function filterSumOfTripledEvents(params) {
+    // func using filter
+}
+
+function reduceSumofTripledEvents(params) {
+    // func using filter
+}
