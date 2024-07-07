5c3e69d416d7c96c9b9a1a7a83e58eb67cb625d1
blister-pack <andrecrujo@hotmail.com>
2024-07-07 18:06:33 +0200

    rewrote sumOfTripledEvens using filter map and reduce

--- 7odin_objects/1js_exercise/sumOfTripledEvens.js
+++ 7odin_objects/1js_exercise/sumOfTripledEvens.js
@@ -1,14 +1,29 @@
 // the objective of this exercise is to rewrite the function
 // using filter, map and reduce
 
-function mapSumOfTripledEvents(params) {
+function sumOfTripledEvents(arr) {
     // func using map
+    let sum = 0;
+    let evenArray = arr.filter(num =>num % 2 === 0);
+    console.log(evenArray);
+
+    let tripledArray = evenArray.map(num => num * 3);
+    console.log(tripledArray);
+    
+    let summedArray = tripledArray.reduce((summed, num) => summed + num, 0);
+
+    return summedArray;
 }
 
-function filterSumOfTripledEvents(params) {
-    // func using filter
+function cleanSumOfTripledEvens(array) {
+    return array
+        .filter((num) => num % 2 === 0)
+        .map((num) => num * 3)
+        .reduce((acc, curr) => acc + curr);
 }
 
-function reduceSumofTripledEvents(params) {
-    // func using filter
-}
+let lol = [1, 2, 3, 4, 5];
+
+sumOfTripledEvents(lol);
+console.log(sumOfTripledEvents(lol));
+console.log(cleanSumOfTripledEvens(lol));
